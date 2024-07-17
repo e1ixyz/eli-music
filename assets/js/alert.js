fa6329536a2ce4ffcbf4c79731d0c9fbced08cb9
e1ixyz <elimcgehee2001@gmail.com>
2024-07-17 14:31:09 -0400

    alert

--- assets/js/alert.js
+++ assets/js/alert.js
@@ -1,6 +1,6 @@
 // Join Alert
 var alerted = localStorage.getItem('alerted') || '';
 if (alerted != 'yes') {
- alert("Welcome! For the optimal user experience, please enable audio in your browser settings. =) All songs are made/remixed by me.");
+ alert("Welcome! For the optimal user experience, please enable audio in your browser settings. =) All songs are !unfinished! demos that are made/remixed by me.");
  localStorage.setItem('alerted','yes');
   }
